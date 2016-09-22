d6d7aa7e41e2383fe0a139edfaeef11b123e0ffd
Thomas Bayer <bayer@predic8.de>
2016-09-22 11:56:33 +0200

    Added Sub

--- src/main/java/de/predic8/Addierer.java
+++ src/main/java/de/predic8/Addierer.java
@@ -1,7 +1,13 @@
 package de.predic8;
 
 public class Addierer {
+
     public int addiere(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
 }
--- src/test/java/de/predic8/AddiererTests.java
+++ src/test/java/de/predic8/AddiererTests.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AddiererTests {
-    Addierer addierer = new Addierer();
+        Addierer addierer = new Addierer();
 
     @Test
     public void testAddiere() throws Exception {
         assertEquals(3, addierer.addiere(1,2));
+        assertEquals(2, addierer.addiere(0,2));
     }
 
 }
